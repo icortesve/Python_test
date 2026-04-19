aa89577eea7dcab70d142667a9a00c4afce28f1f
icortesve <ivan.cortesve@gmail.com>
2026-04-19 15:18:03 -0400

    Encadenamiento métodos

--- Python_test/POO_tarea.py
+++ Python_test/POO_tarea.py
@@ -8,16 +8,19 @@ class Usuario:
   
     def hacer_compra(self, monto):  #recibe como argumento el monto de la compra
         self.saldo_pagar += monto   #el saldo a pagar del usuario aumenta en la cantidad del valor recibido
+        return self
 
 # pagar_tarjeta(self, monto): crea un método que pague un monto de la tarjeta de crédito. haciendo que se reduzca el saldo_pagar.
     def pagar_tarjeta(self, monto):
         self.saldo_pagar -= monto
         if self.saldo_pagar < 0:
             self.saldo_pagar = 0
+        return self
 
 # mostrar_saldo_usuario(self): crea un método que imprima el nombre completo del usuario y el saldo a pagar de su tarjeta.Ejemplo: “Usuario: Nariyoshi Miyagi, Saldo a Pagar: $50”
     def mostrar_saldo_usuario(self):
         print(f"Usuario:{self.nombre} {self.apellido}, Saldo a pagar: ${self.saldo_pagar}")
+        return self
 
 # BONUS: transferir_deuda(self, otro_usuario, monto): crea un método que reduzca la deuda (saldo_pagar) del usuario por el monto, y agrega esa cantidad al saldo_pagar de otro_usuario
     def transferir_deuda(self, otro_usuario, monto):
@@ -26,6 +29,7 @@ class Usuario:
             otro_usuario.saldo_pagar += monto
         else:
             print("No hay suficiente deuda para transferir")
+        return self
 
 # Crea el archivo un Python con la clase Usuario, con la base que te proporcionamos
 # Agrega el método pagar_tarjeta a la clase Usuario
@@ -43,12 +47,19 @@ Usuario1.hacer_compra(600) #Segunda compra
 Usuario1.pagar_tarjeta(200) #Pago
 Usuario1.mostrar_saldo_usuario() #Mostrar saldo
 
+##Encadenamiento:
+Usuario1.hacer_compra(800).hacer_compra(600).pagar_tarjeta(200).mostrar_saldo_usuario()
+
+
 # Haz que el segundo usuario haga 1 compra y luego pague 2 veces su tarjeta. Muestra su saldo
 
 Usuario2.hacer_compra(500) #Primera compra
 Usuario2.pagar_tarjeta(100) #Primer pago
 Usuario2.pagar_tarjeta(200) #Segundo pago
 Usuario2.mostrar_saldo_usuario() #Mostrar saldo
+
+##Encadenamiento:
+Usuario2.hacer_compra(500).pagar_tarjeta(100).pagar_tarjeta(200).mostrar_saldo_usuario()
 
 # Haz que el tercer usuario haga 3 compras y luego pague su tarjeta 4 veces. Muestra su saldo
 
@@ -60,3 +71,6 @@ Usuario3.pagar_tarjeta(50) #Segundo pago
 Usuario3.pagar_tarjeta(80) #Tercer pago
 Usuario3.pagar_tarjeta(100) #Cuarto pago
 Usuario3.mostrar_saldo_usuario() #Mostrar saldo
+
+##Encadenamiento:
+Usuario3.hacer_compra(800).hacer_compra(700).hacer_compra(500).pagar_tarjeta(600).pagar_tarjeta(50).pagar_tarjeta(80).pagar_tarjeta(100).mostrar_saldo_usuario()
