c6a4b47583664ed1e6330dad8ae4f5ac4ea69c82
icortesve <ivan.cortesve@gmail.com>
2026-04-19 19:22:01 -0400

    Commit para agregar trjets creadas

--- Python_test/TarjetaCredito.py
+++ Python_test/TarjetaCredito.py
@@ -35,3 +35,21 @@ class TarjetaCredito:
         print("\n--- Todas las tarjetas ---")
         for i, tarjeta in enumerate(cls.tarjetas, start=1):
             print(f"Tarjeta {i}: Saldo = ${tarjeta.saldo_pagar}")
+
+
+# Crear tarjetas
+tarjeta1 = TarjetaCredito(1000, 0.02)
+tarjeta2 = TarjetaCredito(2000, 0.03)
+tarjeta3 = TarjetaCredito(500, 0.05)
+
+# Tarjeta 1: 2 compras, 1 pago, interés y mostrar (encadenado)
+tarjeta1.compra(200).compra(300).pago(100).cobrar_interes().mostrar_info_tarjeta()
+
+# Tarjeta 2: 3 compras, 2 pagos, interés y mostrar
+tarjeta2.compra(500).compra(400).compra(300).pago(200).pago(100).cobrar_interes().mostrar_info_tarjeta()
+
+# Tarjeta 3: exceder límite
+tarjeta3.compra(200).compra(200).compra(200).compra(200).compra(200).mostrar_info_tarjeta()
+
+# BONUS
+TarjetaCredito.mostrar_todas_las_tarjetas()
