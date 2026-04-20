c7d730cf2a1c02bb0d44fbd07fc9967941684ef7
icortesve <ivan.cortesve@gmail.com>
2026-04-19 21:44:52 -0400

    Agrega métodos a cada clase

--- Python_test/Tamagotchi.py
+++ Python_test/Tamagotchi.py
@@ -3,6 +3,18 @@ class Persona:
         self.nombre = nombre
         self.apellido = apellido
         self.tamagotchi = tamagotchi
+        
+    def jugar_con_tamagotchi(self):
+        print(f"{self.nombre} está jugando con {self.tamagotchi.nombre}")
+        self.tamagotchi.jugar()
+
+    def darle_comida(self):
+        print(f"{self.nombre} le da de comer a {self.tamagotchi.nombre}")
+        self.tamagotchi.comer()
+
+    def curarlo(self):
+        print(f"{self.nombre} está curando a {self.tamagotchi.nombre}")
+        self.tamagotchi.curar()
 
 class Tamagotchi:
     def __init__(self, nombre, color):
@@ -11,3 +23,18 @@ class Tamagotchi:
         self.salud = 100
         self.felicidad = 100
         self.energia = 100
+
+    def jugar(self):
+        self.felicidad += 10
+        self.salud -= 5
+        print(f"{self.nombre} está jugando. Felicidad: {self.felicidad}, Salud: {self.salud}")
+
+    def comer(self):
+        self.felicidad += 5
+        self.salud += 10
+        print(f"{self.nombre} está comiendo. Felicidad: {self.felicidad}, Salud: {self.salud}")
+
+    def curar(self):
+        self.salud += 20
+        self.felicidad -= 5
+        print(f"{self.nombre} se está curando. Salud: {self.salud}, Felicidad: {self.felicidad}")
