ec8b73d3f60a8bc0834769a5491b93e3ce3d3b8f
icortesve <ivan.cortesve@gmail.com>
2026-04-19 21:41:34 -0400

    Primer Commit: Clases declaradas: Persona,Tamagotchi

---
+++ Python_test/Tamagotchi.py
@@ -0,0 +1,13 @@
+class Persona:
+    def __init__(self, nombre, apellido, tamagotchi):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.tamagotchi = tamagotchi
+
+class Tamagotchi:
+    def __init__(self, nombre, color):
+        self.nombre = nombre
+        self.color = color
+        self.salud = 100
+        self.felicidad = 100
+        self.energia = 100
